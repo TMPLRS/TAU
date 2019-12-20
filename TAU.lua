46448632c6cec4ec5475232df02a933141116eb9
TMPLRS <58913415+TMPLRS@users.noreply.github.com>
2019-12-20 19:32:39 +0300

    Add files via upload

--- TAU.lua
+++ TAU.lua
@@ -7207,12 +7207,12 @@ end
 end
 else
 if result.status_.ID == "UserStatusRecently" and result.profile_photo_ ~= false then
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, taha.photos_[0].sizes_[1].photo_.persistent_id_,''..rdphoto..'\n🎟¦ ايديك • '..msg.sender_user_id_..'\n🎭¦ معرفك • '..username..'\n📌¦ موقعك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🌁¦ صورك • ('..photps..')\n🎲¦ تفاعلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, taha.photos_[0].sizes_[1].photo_.persistent_id_,''..rdphoto..'\n🎟¦ ايديك • '..msg.sender_user_id_..'\n🎭¦ معرفك • '..username..'\n📌¦ موقعك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🎲¦ تفاعلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
 else 
 if result.status_.ID == "UserStatusEmpty" and result.profile_photo_ == false then
-send(msg.chat_id_, msg.id_,'[\n🎟¦ ايديك • '..msg.sender_user_id_..'\n🎭¦ معرفك • '..username..'\n📌¦ موقعك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🌁¦ صورك • ('..photps..')\n🎲¦ تفاعلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')]\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
+send(msg.chat_id_, msg.id_,'[\n🎟¦ ايديك • '..msg.sender_user_id_..'\n🎭¦ معرفك • '..username..'\n📌¦ موقعك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🎲¦ تفاعلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')]\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
 else
-send(msg.chat_id_, msg.id_, '\n⚠️┇الصوره ←  ليس لديك صور في حسابك 🍃'..'[\n🎟¦ ايديك • '..msg.sender_user_id_..'\n🎭¦ معرفك • '..username..'\n📌¦ موقعك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🌁¦ صورك • ('..photps..')\n🎲¦ تفاعلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')]\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
+send(msg.chat_id_, msg.id_, '\n⚠️┇الصوره ←  ليس لديك صور في حسابك 🍃'..'[\n🎟¦ ايديك • '..msg.sender_user_id_..'\n🎭¦ معرفك • '..username..'\n📌¦ موقعك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🎲¦ تفاعلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')]\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
 end 
 end
 end
@@ -7230,7 +7230,7 @@ get_id_text = get_id_text:gsub('#game',NUMPGAME)
 get_id_text = get_id_text:gsub('#photos',photps) 
 send(msg.chat_id_, msg.id_,'['..get_id_text..']')   
 else
-send(msg.chat_id_, msg.id_,'[\n🎟¦ ايديــك • ('..msg.sender_user_id_..')\n🎭¦ مـعرفك • '..username..'\n📌¦ موقعـك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🌄¦ عدد صورك • ('..photps..')\n🎲¦ تفاعـلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')]\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
+send(msg.chat_id_, msg.id_,'[\n🎟¦ ايديــك • ('..msg.sender_user_id_..')\n🎭¦ مـعرفك • '..username..'\n📌¦ موقعـك • '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n🎲¦ تفاعـلك • ('..Total_Msg(Msguser)..')\n💌¦ رسائلك • ('..Msguser..')\n🧿¦ سحكاتك • • ('..edit..')\n💎¦مجوهرآتك • ('..NUMPGAME..')]\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉')   
 end
 end
 
